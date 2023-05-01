d5e4ac4422db3ae5e453ff24464b018feeba723b
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-01 15:17:29 +0300

    install tailwind-merge
    fix the problem with overwriting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import Button from "./Button"
+import Button from "./Button";
+import { GoBell, GoEye, GoHome, GoPulse } from "react-icons/go";
 
 function App() {
-    return <div>
+    return (
         <div>
-            <Button primary>Click me!</Button>
+            <div>
+                <Button primary rounded outline>
+                    <GoBell />
+                    Click me!
+                </Button>
+            </div>
+            <div>
+                <Button secondary rounded>
+                    <GoPulse />
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button success outline>
+                    Submit
+                </Button>
+            </div>
+            <div>
+                <Button warning rounded>
+                    <GoEye />
+                    Hide Ads!
+                </Button>
+            </div>
+            <div>
+                <Button danger rounded>
+                    <GoHome />
+                    See Deal!
+                </Button>
+            </div>
         </div>
-        <div>
-            <Button secondary rounded>Buy Now!</Button>
-        </div>
-        <div>
-            <Button success outline>Submit</Button>
-        </div>
-        <div>
-            <Button warning rounded>Hide Ads!</Button>
-        </div>
-        <div>
-            <Button danger outline rounded>See Deal!</Button>
-        </div>
-    </div>
+    )
 };
 
 export default App
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import className from 'classnames';
+import { twMerge } from 'tailwind-merge';
 
 function Button({
     children,
@@ -10,7 +11,7 @@ function Button({
     outline,
     rounded
 }) {
-    const classes = className('px-3 py-1.5 border', {
+    const classes = twMerge(className('flex items-center px-3 py-1.5 border', {
         'border-blue-600 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-800 text-white': secondary,
         'border-green-600 bg-green-500 text-white': success,
@@ -18,12 +19,12 @@ function Button({
         'border-red-600 bg-red-500 text-white': danger,
         'rounded-full': rounded,
         'bg-white': outline,
-        'text-blue-500': outline && primary,
+        'text-blue-600': outline && primary,
         'text-gray-900': outline && secondary,
         'text-green-600': outline && success,
         'text-yellow-600': outline && warning,
         'text-red-600': outline && danger,
-    });
+    }));
 
     return <button className={classes}>{children}</button>
 };
