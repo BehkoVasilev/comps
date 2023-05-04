94b1d93fe9331cc8db2c62c416f3ad0344dcc328
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-04 09:58:33 +0300

    add a click handler to show/hide modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,11 +1,17 @@
 import Modal from "../components/Modal";
 import Button from "../components/Button";
+import { useState } from "react";
 
 function ModalPage() {
+    const [showModal, setSholModal] = useState(false);
+
+    const handleClick = () => {
+        setSholModal((current) => !current)
+    }
     return (
         <div>
-            <Button primary rounded>Open Modal</Button>
-            <Modal />
+            <Button onClick={handleClick} primary rounded>Open Modal</Button>
+            {showModal && <Modal />}
         </div>
     );
 };
