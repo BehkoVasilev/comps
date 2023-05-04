5c99d1629781c4dabdb9b40cca2b1ad8dda73e05
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-04 14:37:13 +0300

    add data to TablePage

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,6 +1,13 @@
 import Table from "../components/Table";
 
 function TablePage () {
+    const data = [
+        { name: 'Orange', color: 'bg-orange-500', score: '5' },
+        { name: 'Apple', color: 'bg-red-500', score: '2' },
+        { name: 'Banana', color: 'bg-yellow-500', score: '1' },
+        { name: 'Lime', color: 'bg-green-500', score: '3' },
+    ]
+
     return (
         <Table />
     );
