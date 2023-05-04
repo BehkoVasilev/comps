5980980bfa4212127cd3065c78a8813771b3089b
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-04 11:25:30 +0300

    add button to the modal
    fixing the bug with scrolling when modul is  open with adding a class to the body "overflow-hidden"

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 
-function Modal({ onClose }) {
+function Modal({ onClose, children }) {
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden');
+
+        return () => {
+            document.body.classList.remove('overflow-hidden')
+        }
+    }, [])
+
     return ReactDOM.createPortal(
         <div>
             <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-            <div className="absolute inset-40 p-10 bg-white">I am modal!</div>
+            <div className="absolute inset-40 p-10 bg-white">
+                {children}
+            </div>
         </div>,
         document.querySelector('.modal-container')
     )
