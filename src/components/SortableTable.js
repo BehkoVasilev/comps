3f26c5b35471131139891031f897dc5bcd7c25dd
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-05 15:22:48 +0300

    add updatedConfig to add is column sortable
    and display it in Table component

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,20 @@
 import Table from "./Table"
 
 function SortableTable(props) {
-    return <Table {...props}/>
+    const { config } = props;
+
+    const updatedConfig = config.map((column) => {
+        if (!column.sortValue) {
+            return column
+        };
+
+        return {
+            ...column,
+            header: () => <th className="p-2">{column.label} Is Sortable</th>
+        }
+    });
+    //when we pass config={updatedConfig} like that the config prop in {...props} will be overwritten
+    return <Table {...props} config={updatedConfig} />
 };
 
-export default SortableTable
+export default SortableTable;
